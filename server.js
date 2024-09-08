828f09e9ed9430cd11766962a368723e8e02e1cc
sameersaini12 <sameersaini35806@gmail.com>
2024-09-08 11:25:39 +0530

    adding hostname

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get("/" , (req,res) => {
 })
 
 const PORT = process.env.PORT || 5000
-httpServer.listen(PORT , ()=> {
+httpServer.listen(PORT ,"0.0.0.0", ()=> {
     console.log(`Listening on PORT ${PORT}`);
     dbConnect()
 })
